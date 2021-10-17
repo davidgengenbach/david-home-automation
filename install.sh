c80d88d52ef8c804eeae559a1859c82863520797
David Gengenbach <info@davidgengenbach.de>
2021-10-17 14:21:07 +0200

    Improves supervisord script

--- install.sh
+++ install.sh
@@ -4,15 +4,8 @@ set -eux
 
 THIS_DIR="$(pwd -P)"
 
-CONF="supervisord.replaced.conf"
-
-sudo ln -s $HOME/.poetry/bin/poetry /usr/bin || true
 sudo ln -s $HOME/.local/bin/eq3.exp /usr/bin || true
 
-$HOME/.poetry/bin/poetry install
-
-echo_supervisord_conf > $CONF
-
-cat supervisord.conf >> $CONF
-sed -i "s|WORKING_DIRECTORY|$THIS_DIR|g" $CONF
-sed -i "s|POETRY|$(which poetry)|g" $CONF
+CONF="/etc/supervisord.conf"
+echo_supervisord_conf | sudo tee $CONF
+cat supervisord.conf | sudo tee -a $CONF
