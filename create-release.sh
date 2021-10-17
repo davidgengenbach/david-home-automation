8f7c086e5898355570890bf400e1739c45f72fa3
David Gengenbach <info@davidgengenbach.de>
2021-10-17 13:00:03 +0200

    Bump version to Bumping version from 0.1.3 to 0.1.4

--- create-release.sh
+++ create-release.sh
@@ -2,8 +2,13 @@
 
 set -eux
 
+if ! git status --porcelain; then
+  echo "Can only run on clean git tree. Please commit and push all your changes. Exiting"
+  exit 1
+fi
+
 # Increase version
-poetry version patch
+TAG=$(poetry version patch)
 
 git add .
 git commit -am "Bump version to $TAG"
